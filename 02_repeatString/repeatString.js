5e53cc8421caefb675c4906670c28a9587e8ac94
Cecilia <ravacecilia1@gmail.com>
2024-08-08 10:33:22 -0300

    feat: Complete the exercise '02_repeatString'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepetitions) {
+    if (numberOfRepetitions < 0){return 'ERROR'};
+    let repetitions = 0;
+    let stringWithRepetition = '';
+    while (repetitions < numberOfRepetitions){
+        stringWithRepetition += string;
+        repetitions += 1;
+    }
+    return stringWithRepetition;
 };
 
 // Do not edit below this line
