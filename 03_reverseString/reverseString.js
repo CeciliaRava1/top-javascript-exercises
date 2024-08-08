a8dad05ffdcac7372fd9b362427d02a21fc60a2d
Cecilia <ravacecilia1@gmail.com>
2024-08-08 10:48:23 -0300

    feat: Complete the exercise '03_reverseString'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let reversedInputString = '';
+    for (var i = inputString.length-1; i >= 0; i--){
+        reversedInputString += inputString[i]
+    }
+    return reversedInputString;
 };
 
 // Do not edit below this line
