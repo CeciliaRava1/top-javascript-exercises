a5e80cf45247f639decd3d616453b3a240e70fa7
Cecilia <ravacecilia1@gmail.com>
2024-08-08 13:02:33 -0300

    feat: Complete the exercise '05_sumAll'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(inputInteger1, inputInteger2) {
+    let sum = 0;
+    if (Number.isInteger(inputInteger1) && Number.isInteger(inputInteger2)){
+        if (inputInteger1 > 0 && inputInteger2 > 0){
+            if (inputInteger1 < inputInteger2){
+                for (let i = inputInteger1; i <= inputInteger2; i++){
+                    sum += i;
+                }
+            } else {
+                for (let i = inputInteger2; i <= inputInteger1; i++){
+                    sum += i;
+                }
+            }
+            
+        } else {
+            return 'ERROR';
+        }
+    } else {
+        return 'ERROR';
+    }
+    return sum;
 };
 
 // Do not edit below this line
