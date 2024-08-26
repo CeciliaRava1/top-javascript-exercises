7e5063d6629db4fc60fb50e58c0f4298a1d44c41
Cecilia <ravacecilia1@gmail.com>
2024-08-26 14:18:32 -0300

    feat: Complete the exercise 'palindromes'

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    newWord = word.split().reverse().join()
+    if (word === newWord){
+        return true;
+    } else {
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
