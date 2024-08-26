3b7dabd94e3597d422b7971ba3c67a34b864e305
Cecilia <ravacecilia1@gmail.com>
2024-08-26 14:10:59 -0300

    feat: Complete the exercise 'calculator'

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,19 +7,39 @@ const subtract = function(num1,num2) {
 };
 
 const sum = function(array) {
-	
+  if (array.length === 0){
+    return 0;
+  }
+  if (array.length === 1){
+    return array[0];
+  } else {
+    result = 0;
+    for(let i=0; i < array.length; i++)
+      result += array[i];
+    return result;
+  }
+  
 };
 
-const multiply = function(num1, num2) {
-  return num1 * num2;
+const multiply = function(array) {
+  let result = array[0];
+  for(let i=1; i < array.length; i++){
+    result *= array[i]
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, numPower) {
+	return num1 ** numPower
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+    let ans = 1; 
+    if(number === 0)
+        return 1;
+    for (let i = 2; i <= number; i++) 
+        ans = ans * i; 
+    return ans; 
 };
 
 // Do not edit below this line
