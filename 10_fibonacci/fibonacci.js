6b8c4729ba118f736b99ca207fa26ad3ef3cd5bb
Cecilia <ravacecilia1@gmail.com>
2024-08-26 14:42:48 -0300

    feat: Progress with the exercise 'fibonacci'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(numberPosition) {
+    firstNumber = 0; secondNumber= 1; result = 0;
+    for(let i = 1; i < numberPosition; i++){
+        result = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = result;
+    }
+    return result;
 };
 
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
