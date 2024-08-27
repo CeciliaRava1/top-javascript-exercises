527ac58b76f1c450d130c5d93ab882ccccfffd05
Cecilia <ravacecilia1@gmail.com>
2024-08-27 08:25:17 -0300

    feat: Complete the exercise 'getTheTitles'

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    arrayWithTiles = [];
+    for (let position in array) {
+        arrayWithTiles.push(array[position].title);
+    }
+    return arrayWithTiles;
 };
 
 // Do not edit below this line
