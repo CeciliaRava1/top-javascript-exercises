98e3a9a8f9dac9c0c03154e72602ecdebb6953ca
Cecilia <ravacecilia1@gmail.com>
2024-08-27 08:17:41 -0300

    feat: Complete the exercise 'fibonacci'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,16 @@
 const fibonacci = function(numberPosition) {
-    firstNumber = 0; secondNumber= 1; result = 0;
-    for(let i = 1; i < numberPosition; i++){
-        result = firstNumber + secondNumber;
-        firstNumber = secondNumber;
-        secondNumber = result;
+    numberPosition = parseInt(numberPosition);
+    if(numberPosition < 0){
+        return 'OOPS';
+    } else if (numberPosition === 1){
+        result = 1;
+    } else {
+        firstNumber = 0; secondNumber= 1; result = 0;
+        for(let i = 1; i < numberPosition; i++){
+            result = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = result;
+        }
     }
     return result;
 };
