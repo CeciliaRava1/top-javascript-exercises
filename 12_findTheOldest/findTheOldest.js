6b1d581dba55a5ba8c84a0f428e116949432966a
Cecilia <ravacecilia1@gmail.com>
2024-08-27 08:59:35 -0300

    feat: Complete the exercise 'findTheOldest'

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    todayYear = new Date();
+    ageOfOldestPerson0 = 0;
+    number = -1
+    for(let position in array){
+        number +=1
+        if(array[position].yearOfDeath === undefined){
+            ageOfOldestPerson1 = todayYear.getFullYear() - array[position].yearOfBirth
+        } else {
+            ageOfOldestPerson1 = array[position].yearOfDeath - array[position].yearOfBirth;
+        }
+        if(ageOfOldestPerson0 < ageOfOldestPerson1){
+            ageOfOldestPerson0 = ageOfOldestPerson1;
+            positionOfOldestPerson = number;
+        }
+        
+    }
+    return array[positionOfOldestPerson];
 
 };
 
